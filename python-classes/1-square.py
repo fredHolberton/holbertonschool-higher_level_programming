25a0f2d67df87c02179f4e63a063c473c113e956
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-11-21 10:39:20 +0000

    1. Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""module : 1. Square with size
+
+
+"""
+
+
+class Square:
+    """This class allows to value a size 
+
+
+    """
+
+
+   def __init__(self, size):
+       """Constructor method
+
+
+       Arg:
+           size (int): The size value of private attribut (default value = 0)
+
+
+       """
+       __size = size
