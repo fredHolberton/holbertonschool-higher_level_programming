94cbdf37331e12dcce2b7267cf3ba44871150696
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-11-21 09:56:48 +0000

    0. My first square

---
+++ python-classes/0-square.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""module : 0. My first square"""
+class Square:
+    """This class is an empty class"""
+    pass
---
+++ python-classes/README.md
@@ -0,0 +1,1 @@
+Contient des tasks sur les classes et les objets Python
