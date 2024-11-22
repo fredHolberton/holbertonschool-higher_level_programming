8284d58248fc74a8e6357c5c0e1e2259286624ae
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-11-22 10:08:33 +0100

    Test 2 Fix 1

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,8 +6,9 @@ class Square:
     """This class allows to value a private property size"""
     def __init__(self, size=0):
         """Constructor method"""
-        if isinstance(size, int):
+        if isinstance(size, int) == False:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise valueError("size must be >= 0")
-        self.__size = size
+        else:
+            self.__size = size
