15d9afa8dc5e412c6c151ea8188946fcf0d3c3bb
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-11-22 07:48:30 +0000

    Test 1 Fix 2

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -6,7 +6,7 @@
 
 
 class Square:
-    """This class allows to value a size 
+    """This class allows to value a private property size 
 
 
     """
@@ -16,9 +16,7 @@ class Square:
         """Constructor method
 
 
-        Arg:
-            size (int): The size value of private attribut (default value = 0)
-
-
         """
+
+
         __size = size
