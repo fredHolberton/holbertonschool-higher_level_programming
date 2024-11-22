4053fb9d81fdb155f9d97ea0d406f6a213877cd0
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-11-22 09:13:21 +0100

    Test 1 Fix 3

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,22 +1,13 @@
 #!/usr/bin/python3
-"""module : 1. Square with size
-
-
-"""
+"""module : 1. Square with size"""
 
 
 class Square:
-    """This class allows to value a private property size 
-
-
-    """
+    """This class allows to value a private property size"""
 
 
     def __init__(self, size):
-        """Constructor method
-
-
-        """
+        """Constructor method"""
 
 
         __size = size
