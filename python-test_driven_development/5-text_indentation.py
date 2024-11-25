a378764f4ee43cd3bca7d012711d6c7852780ec8
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-11-25 16:04:40 +0100

    4. Text indentation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Module : 4. text indentation """
+
+
+def text_indentation(text):
+    """ Method that print a new line after a ., ? or : """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    text = text.strip()
+    i = 0
+    while i < len(text):
+        print(text[i], end='')
+        if text[i] in [".", "?", ":"]:
+            print("\n")
+            i += 1
+        i += 1
