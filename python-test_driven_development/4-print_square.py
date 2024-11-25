ec0181c1ab4c614e4c7a5e2b49f848cfa97e45df
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-11-25 15:24:43 +0100

    Test 3 Fix 2

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -8,9 +8,7 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    if size == 0:
-        print("")
-    else:
+    if size > 0:
         for i in range(1, size + 1):
             for j in range(1, size + 1):
                 if j < size:
