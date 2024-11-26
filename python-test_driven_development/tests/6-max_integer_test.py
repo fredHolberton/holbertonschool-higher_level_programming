61d038fd510755c6d9ec2fc0421f1fc92785ad56
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-11-26 09:42:13 +0100

    5. Max integer - Unittest

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,9 @@
+"""Module to test the task 5. Max integer - Unittest"""
+
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+Class TestMaxInteger(unittest.TestCase):
+    pass 
