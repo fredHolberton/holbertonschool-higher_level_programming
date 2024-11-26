6c6925dce6131d82a001f4c735ba07273bb9d8b3
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-11-26 10:04:17 +0100

    Test 6 Fix 2

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -6,5 +6,11 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    # def test_max_at_the_end(self):
-    pass
+    def test_max_at_the_end(self):
+        self.assertEqual(max_integer([1, 2, 3], 3))
+
+    def test_max_at_the_beginning(self):
+        self.assertEqual(max_integer([3, 1, 2], 3))
+
+    def test_max_in_the_middle(self):
+        self.assertEqual(max_integer([2, 3, 1], 3))
